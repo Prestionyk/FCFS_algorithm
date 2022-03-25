5c9448b44a64c8498f35137102da42324ae78e88
Mariusz Skuza <72551592+Prestionyk@users.noreply.github.com>
2022-03-25 16:19:58 +0100

    Add files via upload

---
+++ AlgorithmFCFS/Process.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace AlgorithmFCFS
+{
+    class Process
+    {
+        public static int idProcess = 0, colorId = 0;
+
+        bool isDone = false;
+        int Id, workTime, workLeft,waitTime = 0;
+        ConsoleColor color;
+
+        public Process(int time)
+        {
+            Id = idProcess++;
+            workTime = time;
+            workLeft = workTime;
+            color = GetNextColor();
+        }
+        public void ExecutingForOneQTime()
+        {
+            workLeft--;
+            Console.ForegroundColor = color;
+            Console.WriteLine($"QTime:{Program.QTime++}  \tProcess {Id} working...");
+
+            if (workLeft == 0)
+                WorkDone();
+        }
+        void WorkDone()
+        {
+            isDone = true;
+            Console.ForegroundColor = color;
+            Console.WriteLine($"QTime:{Program.QTime}  \tProcess {Id} done his work");
+        }
+
+        public bool IfMustWork()
+        {
+            if (isDone)
+                return false;
+            else
+                return true;
+        }
+        public void Wait()
+        {
+            waitTime++;
+        }
+
+        public int getWait()
+        {
+            return waitTime;
+        }
+        public void ShowWaitTime()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\nProcess {Id} waited {waitTime} QT");
+            Console.BackgroundColor = color;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            for (int i = 0; i < waitTime; i++)
+                Console.Write(' ');
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public void ShowRunningTimeBar()
+        {
+            Console.BackgroundColor = color;
+            Console.ForegroundColor = ConsoleColor.Black;
+            for (int i = 0; i < workTime; i++)
+            {
+                if(i == workTime / 2)
+                    Console.Write(Id);
+                else
+                    Console.Write(' ');
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        static ConsoleColor GetNextColor()
+        {
+            ConsoleColor tempColor = (ConsoleColor)(((int)Console.ForegroundColor + colorId++) % Enum.GetValues(typeof(ConsoleColor)).Length);
+            if (tempColor == ConsoleColor.Black)
+                tempColor = (ConsoleColor)(((int)Console.ForegroundColor + colorId++) % Enum.GetValues(typeof(ConsoleColor)).Length);
+
+            return tempColor;
+        }
+    }
+}
---
+++ AlgorithmFCFS/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AlgorithmFCFS
+{
+    class Program
+    {
+        
+        public static int QTime = 0;
+        public static int AdditionalProcess = 10;
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //Queue of process
+        public static List<Process> queue = new()
+        {
+            new Process(3),
+            new Process(5),
+            new Process(4),
+            new Process(17),
+            new Process(7),
+            new Process(6)
+        };
+
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(500, Console.BufferHeight);
+
+            List<Process> doneTasks = new();
+            Thread addProcess = new(Simulate);
+            addProcess.Start();
+
+            //////////////////////////////////////////////////////////////////////////////////////
+            //Procesor work simulation
+            Process inProcesor;
+            do
+            {
+                if(queue.Count > 0)
+                {
+
+                    inProcesor = queue[0];
+                    queue.RemoveAt(0);
+                    do
+                    {
+                        inProcesor.ExecutingForOneQTime();
+                        Thread.Sleep(100);
+                        queue.ForEach(Wait);
+                    } while (inProcesor.IfMustWork());
+
+                    doneTasks.Add(inProcesor);
+                }
+            } while (queue.Count != 0 || AdditionalProcess > 0);
+
+            //////////////////////////////////////////////////////////////////////////////////////
+            //Summary of simulation
+            Console.WriteLine();
+
+            int avgTime = 0;
+
+            foreach(Process p in doneTasks)
+            {
+                avgTime += p.getWait();
+                p.ShowWaitTime();
+            }
+
+            avgTime /= doneTasks.Count;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\n\nAverage waiting time: {avgTime}");
+            Console.WriteLine("\nProcess execution timeline:");
+
+            foreach (Process p in doneTasks)
+            {
+                p.ShowRunningTimeBar();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //////////////////////////////////////////////////////////////////////////////////////
+            //Aditional functions
+            static void Wait(Process p)
+            {
+                p.Wait();
+            }
+
+            static void Simulate()
+            {
+                Random rnd = new();
+                do
+                {
+                    if (AdditionalProcess != 0)
+                    {
+                        queue.Add(new Process(rnd.Next(6) + 3));
+                        AdditionalProcess--;
+                    }
+                    else return;
+                } while (true);
+            }
+        }
+    }
+}
